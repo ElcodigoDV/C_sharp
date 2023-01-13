8d38260df3d286a6fb04b41318e712b07411f00e
Kurban Dmitriy V <d_kurban@yahoo.com>
2023-01-13 22:40:53 +1000

    add new Random() func

--- Example003_Summ/Program.cs
+++ Example003_Summ/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA=30;
-int numberB=5;
+﻿int numberA=new Random().Next(1, 10); //дает случ. число от 1 до 10 - 10 не входит в диапазон
+Console.WriteLine("numberA: "+ numberA);
+int numberB=new Random().Next(1, 10);
+Console.WriteLine("numberB: "+ numberB);
 int result = numberA+numberB;
-Console.WriteLine(result);
+Console.WriteLine("Sum: "+ result);
