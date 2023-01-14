204bebe65a50cb30399160d6bda37d8b73465495
Kurban Dmitriy V <d_kurban@yahoo.com>
2023-01-15 02:39:31 +1000

    DZ#2 is done

--- lesson_1_ex_2/Program.cs
+++ lesson_1_ex_2/Program.cs
@@ -8,8 +8,19 @@
 int a = 5;
 int b = 7;
 
-int a = 2;
-int b = 10;
+Console.WriteLine("Ищем max из " + a + " и " + b);
+Console.WriteLine("Максимальное значение " + Math.Max(a, b));
+Console.WriteLine("");//разделяем пустой строкой
 
-int = -9;
-int b = -3;
+int c = 2;
+int d = 10;
+
+Console.WriteLine("Ищем max из " + c + " и " + d);
+Console.WriteLine("Максимальное значение " + Math.Max(a, b));
+Console.WriteLine("");//разделяем пустой строкой
+
+int e = -Math.Abs(9); // abc = -Math.Abs(abc);
+int f = -Math.Abs(3);
+
+Console.WriteLine("Ищем max из " + e + " и " + f);
+Console.WriteLine("Максимальное значение " + Math.Max(e, f));
