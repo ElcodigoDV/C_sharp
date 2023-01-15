97337b1db1354d49da9fe395983493eb2923d788
Kurban Dmitriy V <d_kurban@yahoo.com>
2023-01-16 02:48:31 +1000

    Урок 1 Задание 8 выполнено

--- lesson_1_ex_8/Program.cs
+++ lesson_1_ex_8/Program.cs
@@ -5,41 +5,21 @@
 
 Console.WriteLine("insert number");
 goto_insert: string str = Console.ReadLine(); //сюда возвращает go to, если не число
-List<int> output = new List<int>();
+
 if (Int32.TryParse(str, out int x) == true)
     {
      int num = Int32.Parse(str);
-    //  List<int> output = new List<int>();
-     //  int [] array; //объявляем массив размером num
-     int j = 0;
-     for (int i = 1; i < num +1; i++) //"num+1", чтобы число последнее также попало в проверку
+    
+    for (int i = 1; i < num+1; i++) //"num+1", чтобы число последнее также попало в проверку
          {
-         if ((num % 2) == 0)
+         if ((i % 2) == 0)
              {
-              output = output.Add(j);
+            Console.Write(i + " ");
              }
          else
              {
-             //  Console.WriteLine("Число " + num + " нечетное.\n");   
+             //  nothing   
                   
              }
          }
     }
-Console.WriteLine(output);
-// Console.WriteLine(string.Join(",", arr));//"Твое число " + num + " и оно четное.\n");
-
-
-//         if ((num % 2) == 0)
-//             {
-//                 Console.Write("Твое число " + num + " и оно четное.\n");
-//             }
-//         else
-//             {
-//                 Console.WriteLine("Число " + num + " нечетное.\n");
-//             }
-//     }
-// else
-//     {
-//         Console.WriteLine("Непохоже на число. Попробуй еще раз...");
-//         goto goto_insert; //просим снова ввести число
-//     }
