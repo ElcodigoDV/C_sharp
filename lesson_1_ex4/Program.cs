2c8d46dd28ad81a5e5d44b2bf0fd08efdc73e0c4
Dmitriy Kurban <d_kurban@yahoo.com>
2023-01-15 20:53:06 +1000

    Задание 4 урока 1 выполнено. Можно расслабиться

--- lesson_1_ex4/Program.cs
+++ lesson_1_ex4/Program.cs
@@ -5,80 +5,32 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-int [] numbers1 = {2, 3, 7};
-int [] numbers2 = {44, 5, 78};
-int [] numbers3 = {22, 3, 9};
+int[][] numbers = {   //создаем "массив из массивов"
+    new int[] {12, 3, 7},   // 2, 3, 7 -> 7
+    new int[] {44, 5, 78},  // 44 5 78 -> 78
+    new int[] {22, 3, 9}    // 22 3 9 -> 22
+};
 
-int length1 = numbers1.Length; //определяем длину м-ва
-
-int length3 = numbers3.Length; //определяем длину м-ва
-
-// ДЛЯ ПЕРВОГО МАССИВА
-Console.WriteLine("ищем макс. для " + numbers1 + ". Размер массива " + length1 +" элементов" );//выводим в консоль длину массива
-
-
-int i = 0;
-int max1 = numbers1[0]; //numbers1 заменен на element
-while (i < length1)
+foreach(int[] row in numbers)
 {
-//    int max = numbers1[0];
-    if (numbers1[i] > max1)
-    {
-        max1 = numbers1[i];
-    }
-    else
-    {
-        //max = numbers1[]
-    }
-    i++;
+    int length1 = numbers.Length; //определяем длину м-ва
+    Console.WriteLine("ищем макс. для " + string.Join(", ", row) + ". Размер массива " + length1 +" элементов." );//выводим в консоль справ. информацию
+    //Console.WriteLine(length1);
+    int i = 0;
+    int max = row[0];
+    while (i < length1)
+        {
+        if (row[i] > max)
+            {
+            max = row[i];
+            }
+        else
+            {
+            //max = numbers1[]
+            }
+        i++;
+        }
+
+    Console.WriteLine("Макс. значение  " + max);
+    Console.WriteLine(""); //строка-разделитель
 }
-
-Console.WriteLine(max1);
-Console.WriteLine(""); //строка-разделитель
-
-// ДЛЯ ВТОРОГО МАССИВА
-int length2 = numbers2.Length; //определяем длину м-ва
-Console.WriteLine("ищем макс. для " + numbers2 + ". Размер массива " + length2 +" элементов" );//выводим в консоль длину массива
-
-//int length2 = numbers2.Length; //определяем длину м-ва
-int k = 0;
-int max2 = numbers2[0]; //numbers1 заменен на element
-while (k < length2)
-{
-//    int max = numbers1[0];
-    if (numbers2[i] > max2)
-    {
-        max2 = numbers2[i];
-    }
-    else
-    {
-        //max = numbers1[]
-    }
-    k++;
-}
-
-Console.WriteLine(max2);
-Console.WriteLine(""); //строка-разделитель
-
-// ДЛЯ ТРЕТЬЕГО МАССИВА
-Console.WriteLine("ищем макс. для " + numbers3 + ". Размер массива " + length3 +" элементов" );//выводим в консоль длину массива
-
-//int length3 = numbers3.Length; //определяем длину м-ва
-int j = 0;
-int max3 = numbers3[0]; //numbers1 заменен на element
-while (j < length3)
-{
-//    int max = numbers1[0];
-    if (numbers3[i] > max3)
-    {
-        max3 = numbers3[i];
-    }
-    else
-    {
-        //max = numbers1[]
-    }
-    i++;
-}
-
-Console.WriteLine(max3);
-Console.WriteLine(""); //строка-разделитель
