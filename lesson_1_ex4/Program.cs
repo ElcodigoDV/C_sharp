baad301831cdacd620d626416ba34b7472a3cf0d
Kurban Dmitriy V <d_kurban@yahoo.com>
2023-01-15 14:21:23 +1000

    Попытка сделать foreach для вложенного масива

--- lesson_1_ex4/Program.cs
+++ lesson_1_ex4/Program.cs
@@ -8,13 +8,19 @@
 int [] numbers1 = {2, 3, 7};
 int [] numbers2 = {44, 5, 78};
 int [] numbers3 = {22, 3, 9};
-int length1 = numbers1.Length; //определяем длину м-ва
 
-Console.WriteLine("ищем макс. для [2, 3, 7]. Размер массива " + length1 +" элементов" );//выводим в консоль длину массива
+var [] array_numbers = {numbers1, numbers2, numbers3}; // делаем массив из массивов
+Console.WriteLine(array_numbers);
 
-int i = 0;
-int max1 = numbers1[0];
-while (i < length1)
+foreach (int element in array_numbers)
+{
+    int length1 = element.Length; //определяем длину м-ва
+
+    Console.WriteLine("ищем макс. для " + element + ". Размер массива " + length1 +" элементов" );//выводим в консоль длину массива
+
+    int i = 0;
+    int max1 = element[0]; //numbers1 заменен на element
+    while (i < length1)
 {
 //    int max = numbers1[0];
     if (numbers1[i] > max1)
@@ -28,6 +34,6 @@ while (i < length1)
     i++;
 }
 
-Console.WriteLine(max1);
-Console.WriteLine("") //строка-разделитель
-
+    Console.WriteLine(max1);
+    Console.WriteLine(""); //строка-разделитель
+}
