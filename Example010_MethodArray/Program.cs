ffd9c948678b18128172aa58127644f4222cfbc8
Kurban Dmitriy V <d_kurban@yahoo.com>
2023-01-20 02:02:09 +1000

    примеры с массивами 08 09 10 11. создание функций, работа с массивами

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 24, 3, 44, 5, 64, 44, 18 };
+int n = array.Length;
+int find = 44;
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
